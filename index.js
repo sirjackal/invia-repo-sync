d9aa8cdb8ccec53b46030b0c0c54884f3faabfa3
Jan Krepsky <32548166+sirjackal@users.noreply.github.com>
2020-03-19 15:55:55 +0100

    Ignore paths fix

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const sshClient = require('ssh2').Client;
 const notifier = require('node-notifier');
 const readlineSync = require('readline-sync');
 
+const SEVERITY_INFO = 'info';
+const SEVERITY_WARNING = 'warning';
+const SEVERITY_ERROR = 'error';
+
 // TODO: determine default SSH keys path based on OS - process.platform === "win32"
 const sshKeyPath = `${process.env['USERPROFILE']}\\.ssh\\id_rsa`;
 const basePath = path.resolve(__dirname, '..');  // c:\Dev\invia
@@ -18,7 +22,7 @@ console.log('sshKeyPath:', sshKeyPath);
 
 const watchedDirs = [
    'web',
-   //'library/Invia/**'
+   'library/Invia/**'
 ];
 
 for (dir of watchedDirs) {
@@ -43,27 +47,52 @@ const getCurrentDirName = () => {
     return __dirname.substr(i >= 0 ? i + 1 : 0);
 };
 
+const ignoredPaths = [getCurrentDirName() + '/*', '.git', 'node_modules', '.idea', 'dist', 'cache'];
+const ignoredExtensions = ['png', 'jpg', 'gif', 'svg', 'ico'];
+
+let watcherReady = false;
+
 const watchOptions = {
     cwd: basePath,
-    ignored: [getCurrentDirName() + '/*', '.git/**', '**/node_modules/**', '**/*.{png,jpg,gif,svg,ico}'],
+    //ignored: [getCurrentDirName() + '/*', '**/.git/**', '**/node_modules/**', '**/*.{png,jpg,gif,svg,ico}', '**/.idea/**', '**/dist/**', '**/cache/**'],
+    ignored: path => {
+        for (const ignoredPath of ignoredPaths) {
+            if (path.includes(`/${ignoredPath}/`)) {
+                if (!watcherReady) {
+                    console.log(`ignore ${path}`);
+                }
+                return true;
+            }
+        }
+
+        for (const ignoredExtension of ignoredExtensions) {
+            if (path.endsWith(`.${ignoredExtension}`)) {
+                if (!watcherReady) {
+                    console.log(`ignore ${path}`);
+                }
+                return true;
+            }
+        }
+
+        return false;
+    },
     followSymlinks: false
 };
 
-let ready = false;
 const watcher = chokidar.watch(watchedDirs, watchOptions);
 watcher.on('ready', () => {
-    ready = true;
+    watcherReady = true;
     const msg = 'Initial scan complete!';
     console.log(msg);
-    notify(msg);
+    notify(msg, SEVERITY_INFO);
 });
 
 watcher.on('all', (event, winRelPath) => {
     try {
         let winPath = path.resolve(basePath, winRelPath);
-        
-        if (!ready) {
-            console.log(event, winPath);
+        console.log(event, winPath);
+
+        if (!watcherReady) {
             return;
         }
 
@@ -84,13 +113,13 @@ watcher.on('all', (event, winRelPath) => {
             default:
                 const msg = `Unsupported event '${event}'`;
                 console.warn(msg);
-                notify(msg);
+                notify(msg, SEVERITY_WARNING);
                 break;
         }
     }
     catch (err) {
         console.error(err);
-        notify('Error: ' + err.toString());
+        notify('Error: ' + err.toString(), SEVERITY_ERROR);
     }
 });
 
@@ -101,14 +130,14 @@ const scpCopy = (winPath, linuxPath) => {
         } else {
             const msg = `File '${linuxPath}' copied OK`;
             console.log(msg);
-            notify(msg);
+            notify(msg, SEVERITY_INFO);
         }
     });
 }
 
 const sshDelete = (event, linuxPath) => {
     let conn = new sshClient();
-    conn.on('ready', () => {
+    conn.on('watcherReady', () => {
         conn.exec(`rm -rf ${linuxPath}`, function(err, stream) {
             if (err) {
                 throw new Error(`File '${linuxPath}' delete error: ${err}`);
@@ -116,7 +145,7 @@ const sshDelete = (event, linuxPath) => {
             stream.on('close', function(code, signal) {
                 const msg = `${event} '${linuxPath}'`;
                 console.log(msg);
-                notify(msg);
+                notify(msg, SEVERITY_INFO);
                 conn.end();
             }).on('data', function(data) {
                 
@@ -127,11 +156,22 @@ const sshDelete = (event, linuxPath) => {
     }).connect(sshOptions);
 }
 
-const notify = (msg) => {
-    notifier.notify({
+const notify = (msg, severity) => {
+	severity = severity !== undefined ? severity : SEVERITY_INFO;
+
+	const options = {
         title: 'invia-repo-sync',
         message: msg,
         icon: path.join(__dirname, 'invia-logo.png'),
-        wait: false
-    });
+        wait: false,
+        //sound: 'ms-winsoundevent:Notification.Default',
+        sound: severity !== SEVERITY_INFO,
+    };
+
+    notifier.notify(options);
+ //    new notifier.NotificationCenter(options).notify();
+	// new notifier.NotifySend(options).notify();
+	// new notifier.WindowsToaster(options).notify(options);
+	// new notifier.WindowsBalloon(options).notify(options);
+	// new notifier.Growl(options).notify(options);
 };
